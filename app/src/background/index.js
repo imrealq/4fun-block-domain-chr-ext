fd7ac3da59ded7a5281bf258d5c646f8a62f6859
imrealq <quynhtn29@gmail.com>
2024-11-23 22:36:40 +0700

    block example page

--- app/src/background/index.js
+++ app/src/background/index.js
@@ -1,2 +1,26 @@
 console.log('background is running')
 
+// TODO: get blocked urls
+
+chrome.declarativeNetRequest.getDynamicRules().then((rules) => {
+  const ruleIds = rules.map((rule) => rule.id)
+  const maxId = Math.max(ruleIds, 0)
+
+  // TODO: Validate number of rules against MAX_NUMBER_OF_STATIC_RULESETS
+  // Docs: https://developer.chrome.com/docs/extensions/reference/api/declarativeNetRequest#limits
+
+  chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: ruleIds,
+    addRules: [{
+      "id": maxId + 1,
+      "priority": 1,
+      "action": {
+        "type": "block"
+      },
+      "condition": {
+        "urlFilter": "||example.com",
+        "resourceTypes": ["main_frame"]
+      }
+    }]
+  })
+})
