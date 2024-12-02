8cd1e0d3c914322784ee979a698476769b665588
imrealq <quynhtn29@gmail.com>
2024-12-03 00:33:25 +0700

    fix count down

--- app/src/popup/Timer.vue
+++ app/src/popup/Timer.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
-import { getTimeLeft, getIsBlocking } from '@/background/storage.js'
+import { getTimeLeft } from '@/background/timer.js'
+import {getIsBlocking} from '@/background/storage.js'
 
 const timeLeft = ref(0)
 let timer
@@ -8,37 +9,37 @@ let timer
 const formatTime = (ms) => {
  const minutes = Math.floor(ms / 60000)
  const seconds = Math.floor((ms % 60000) / 1000)
- return `${minutes}:${seconds.toString().padStart(2, '0')}` 
-}
-
-const startTimer = async () => {
- updateTimer()
- timer = setInterval(updateTimer, 1000)
-}
-
-const stopTimer = () => {
- clearInterval(timer)
- timeLeft.value = 0
+ return `${minutes}:${seconds.toString().padStart(2, '0')}`
 }
 
 const updateTimer = async () => {
  timeLeft.value = await getTimeLeft()
+ chrome.storage.local.set({ timeLeft: timeLeft.value })
 }
 
 onMounted(async () => {
- const isBlocking = await getIsBlocking()
- if (isBlocking) startTimer()
+ const { isBlocking } = await getIsBlocking()
+ if (isBlocking) {
+   updateTimer()
+   timer = setInterval(updateTimer, 1000)
+ }
 
  chrome.storage.onChanged.addListener((changes) => {
-   if (changes.isBlocking?.newValue) {
-     startTimer()
-   } else {
-     stopTimer()
+   if (changes.isBlocking) {
+     if (changes.isBlocking.newValue) {
+       updateTimer()
+       timer = setInterval(updateTimer, 1000)
+     } else {
+       clearInterval(timer)
+       timeLeft.value = 0 
+     }
    }
  })
 })
 
-onUnmounted(() => stopTimer())
+onUnmounted(() => {
+ clearInterval(timer) 
+})
 </script>
 
 <template>
